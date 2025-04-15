19abab321bdc19a8b7881591bd0101115865abf7
James Martins <james.mendes@gmail.com>
2025-04-14 23:19:06 -0700

    Initial Setup

---
+++ KingsClubApp/app/src/main/java/br/com/android/kingsclubapp/utils/Constants.kt
@@ -0,0 +1,76 @@
+package br.com.android.kingsclubapp
+
+
+/**
+ * MARCO 2024
+ * VERSÃO 1.0 HIBRIDA  BUILD 1
+ * TELA DE INTRO(HOME) NATIVA
+ * TELA DE LOGIN NATIVA
+ * DEMAIS VIEWS = TELA GENERICA COM TOOLBAR + WEBVIEW PARA DEMAIS TELAS
+ *
+ */
+
+/**
+ * Url services dynamic app data
+ */
+const val mUrlGeneralBase = "https://adm.bunkerapp.com.br/wsjson"
+const val mEndPointConfig = "/APP.do"
+const val mEndPointAuth = "/Auth.do"
+const val mEndPointValue = "/VerificaSaldo.do"
+const val mEndPointVicheles = "/Veiculos_App.do"
+
+/**
+ * Url services to static functions
+ */
+const val baseURL = "https://adm.bunkerapp.com.br/app/"
+// Login
+const val mUrlAuthApp = "https://adm.bunkerapp.com.br/wsjson/authApp.do"
+// cadastro
+const val mUrlCadastro = "consulta_V2.do?key=0keurq3V0gU¢&t=q9HO3i0gp£Ewp£XCk1Kl6BJAFqp6wNu6sD"
+const val mUrlCadastro2 = "consulta_V2.do?key=0keurq3V0gU¢"
+// recuperaçào de senha
+const val mUrlRecuperacaoSenha = "recuperacaoSenha.do?key=0keurq3V0gU¢&t=CxWc6B4DViUXRLKgEsQ6mLjsfKY9o6UE&cpf="
+const val mUrlRecuperacaoSenha2 = "recuperacaoSenha.do?key=0keurq3V0gU¢&t=CxWc6B4DViUXRLKgEsQ6mLjsfKY9o6UE&cpf="
+// parceiro
+const val mUrlParceiro = "parceiro.do?key=0keurq3V0gU¢&t=SzwrGPtfs£s£UXRLKgEsQ6mDRkKtMIiTcG"
+const val mUrlParceiro2 = "parceiro.do?key=0keurq3V0gU¢"
+// fale cosnoco
+const val mUrlFaleConosco = "faleConosco.do?key=0keurq3V0gU¢&t=SzwrGPtfs£s£UXRLKgEsQ6mDRkKtMIiTcG"
+const val mUrlFaleConosco2 = "faleConosco.do?"
+// novoMenu (navigation view)
+const val mUrl_NOVO_MENU = "https://adm.bunkerapp.com.br/app/novoMenu.do"
+
+
+/**
+ * Url service lagend app
+ */
+// 15/09/2022
+const val mUrlStatic = "https://adm.bunkerapp.com.br/app/intro.do?key=0keurq3V0gU¢"
+
+/**
+ * One Signal/ Rede duque Web servives
+ */
+const val mUrlUserSearchKeyData = "https://adm.bunker.mk/wsjson/ConsultaCli.do"
+const val mUrlUserPushDataInformation = "https://adm.bunker.mk/wsjson/TokenAppPush.do"
+const val FIRST_LOGIN_DONE = "FIRST_LOGIN_DONE"
+const val VERIFY_KEY_CUSTOMER = "VERIFY_KEY_CUSTOMER"
+const val PROJECT_ID = 219  // ID NUMBER REDE DUQUE PROJECT
+
+// Antiga
+// 2018
+//val mUrlStatic = "http://adm.bunker.mk/appduque/"
+
+// atual nova
+// 15/09/2022
+//val mUrlStatic = "https://adm.bunkerapp.com.br/appduque/"
+
+const val CODE_AUTHETICATION = "VVNOMFZHeldxMkVJR1JCWmZkNVpxMU1icHFGRzhROHlXUWZrTnowVEQ4Y0VqekFGWURIUEt3wqLCog=="
+const val CODE_SENDTOKENAPP = "c8KjYkxyemVUaGd4cjRaTUFkcUJNVEZrSE5uZFVEcXgxQVAzYmVRMzBhRmFwNVhpMnhERFpNSGFHSWFvZDRqNHQ3RE9xU1BoWGhFdDDCog%3D%3D"
+const val CODE_CONSULTACLI = "c8KjYkxyemVUaGd4cjRaTUFkcUJNVEZrSE5uZFVEcXgxQVAzYmVRMzBhRmFwNVhpMnhERFpNSGFHSWFvZDRqNHQ3RE9xU1BoWGhFdDDCog%3D%3D"
+
+
+
+
+
+
+                  
---
+++ KingsClubApp/app/src/main/java/br/com/android/kingsclubapp/utils/Utils.kt
@@ -0,0 +1,58 @@
+package br.com.android.kingsclubapp
+
+import android.Manifest
+import android.content.Context
+import android.net.ConnectivityManager
+import android.webkit.CookieManager
+
+object Utils {
+    private val SP_FILE_NAME = "br.com.android.kingsclubapp"
+    val REQUEST_PERMISSION_CODE = 7
+
+    fun isNetworkConnected(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return (cm.activeNetworkInfo != null
+                && cm.activeNetworkInfo!!.isAvailable
+                && cm.activeNetworkInfo!!.isConnected)
+    }
+
+    fun getCookie(siteName: String, CookieName: String): String? {
+        var CookieValue: String? = null
+
+        val cookieManager = CookieManager.getInstance()
+        val cookies = cookieManager.getCookie(siteName)
+        val temp = cookies.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        for (ar1 in temp) {
+            if (ar1.contains(CookieName)) {
+                val temp1 = ar1.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                CookieValue = temp1[1]
+                break
+            }
+        }
+        return CookieValue
+    }
+
+    fun saveToPreference(context: Context, key: String, value: String) {
+        val sharedPreferences = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun saveToPreference(context: Context, key: String, value: Boolean) {
+        val sharedPreferences = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun readFromPreferences(context: Context, key: String, defaultValue: String): String? {
+        val sharedPreferences = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key, defaultValue)
+    }
+
+    fun readFromPreferences(context: Context, key: String, defaultValue: Boolean): Boolean? {
+        val sharedPreferences = context.getSharedPreferences(SP_FILE_NAME, Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean(key, defaultValue)
+    }
+}
